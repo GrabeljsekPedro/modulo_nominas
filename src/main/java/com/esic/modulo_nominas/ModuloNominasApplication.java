736a500223bcfbe8c3fec44140163819b5f5bf1c
GrabeljsekPedro <grabeljsekpedro@gmail.com>
2025-01-14 08:47:22 +0100

    Initial commit

---
+++ src/main/java/com/esic/modulo_nominas/ModuloNominasApplication.java
@@ -0,0 +1,13 @@
+package com.esic.modulo_nominas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloNominasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuloNominasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esic/modulo_nominas/ModuloNominasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esic.modulo_nominas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloNominasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
